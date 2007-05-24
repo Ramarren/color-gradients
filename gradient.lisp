3d189eeadb765bd5e447ede9a56c27bab25ba677
ramarren <ramarren@16bf030e-5730-0410-874a-e6e7f71daa69>
2007-05-24 16:07:49 +0000

    Dithering
    git-svn-id: file:///home/SVNs/LispSvn/Gradient/trunk@76 16bf030e-5730-0410-874a-e6e7f71daa69

--- gradient.lisp
+++ gradient.lisp
@@ -11,28 +11,51 @@
 	(yd (- y2 y1)))
     (sqrt (+ (* xd xd) (* yd yd)))))
 
-(defun calc-col (d1 d2 c1 c2)
+(defun calc-col (d1 d2 c1 c2 dither)
   (if (zerop d1) 
     c1
     (if (zerop d2) 
       c2
       (let ((d (+ d1 d2)))
 	(flet ((wav (a b)
-	  (/ (+ (* a d1)(* b d2)) d))))
-	  (declare (inline wav))
-	  (mapcar #'wav c1 c2)))))
+	         (/ (+ (* a d1)(* b d2)) d))
+	       (wav-dither (a b)
+	         (+ (/ (+ (* a d1)(* b d2)) d) (- (random dither) (/ dither 2)))))
+	  (declare (inline wav wav-dither))
+	  (mapcar #'(lambda (x) (max 0 (min x 255)))
+		  (mapcar #'round 
+			  (mapcar (if (zerop dither) #'wav #'wav-dither) c2 c1))))))))
       
 
-(defun gradient-horiz (w h x1 x2 y c1 c2) )
+(defun gradient-horiz (w h x1 x2 c1 c2 dither) 
+  (declare (inline calc-col))
+  (let ((data (make-array (list w h))))
+    (dotimes (j h)
+      (let ((val (calc-col (abs (- j x1)) (abs (- j x2)) c1 c2 dither)))
+	(dotimes (i w)
+	  (setf (aref data i j) val))))
+    data))
 
-(defun gradient-verti (w h x y1 y2 c1 c2) )
+(defun gradient-verti (w h y1 y2 c1 c2 dither)
+  (declare (inline calc-col))
+  (let ((data (make-array (list w h))))
+    (dotimes (i w)
+      (let ((val (calc-col (abs (- i y1)) (abs (- i y2)) c1 c2 dither)))
+	(dotimes (j h)
+	  (setf (aref data i j) val))))
+    data))
 
-(defun gradient-diag (w h x1 y1 x2 y2 c1 c2)
+(defun gradient-diag (w h x1 y1 x2 y2 col1 col2 dither)
   (declare (inline cacl-col dist))
-  (if (and (= x1 x2)(= y1 y2)) (error "Gradient points must be different"))
   (let ((data (make-array (list w h)))
-	(A (/ (- y1 y2)(- (* y2 x1)(* x2 y1))))
-	(B (/ (- x2 x1)(- (* y2 x1)(* x2 y1)))))
+	(A (cond
+	     ((and (zerop x1)(zerop y1)) (/ y2 x2))
+	     ((and (zerop x2)(zerop y2)) (/ y1 x1))
+	     (t (/ (- y1 y2)(- (* y2 x1)(* x2 y1))))))
+	(B (cond
+	     ((or (and (zerop x1)(zerop y1))
+		  (and (zerop x2)(zerop y2))) -1)	     
+	     (t (/ (- x2 x1)(- (* y2 x1)(* x2 y1)))))))
     (let ((C1 (/ (* A B) (+ (* A A) (* B B))))
 	  (C2 (- y1 (* (/ B A) x1)))
 	  (C4 (- y2 (* (/ B A) x2)))
@@ -46,11 +69,17 @@
 	      (setf (aref data i j) (calc-col 
 				      (dist i j (* C1 (- V1 C2)) (- (* C3+ V1) C5))
 				      (dist i j (* C1 (- V1 C4)) (- (* C3+ V1) C6))
-				      c1 c2)))))))))
+				      col1 col2 dither)))))))
+    data))
 
-(defun gradient (w h x1 y1 x2 y2 c1 c2) )
+(defun gradient (w h x1 y1 x2 y2 c1 c2 dither) 
+  (if (and (= x1 x2)(= y1 y2)) (error "Gradient points must be different"))
+  (cond
+    ((= x1 x2) (gradient-verti w h y1 y2 c1 c2 dither))
+    ((= y1 y2) (gradient-horiz w h x1 x2 c1 c2 dither))
+    (t (gradient-diag w h x1 y1 x2 y2 c1 c2 dither))))
 
 (defun plot-gradient (bitmap gdata)
   (dotimes (i (array-dimension gdata 0))
     (dotimes (j (array-dimension gdata 1))
-      (apply #'pixel-rgba `(,bitmap i j ,@(aref gdata i j) 255)))))
+      (apply #'pixel-rgba `(,bitmap ,i ,j ,@(aref gdata i j) 255)))))
