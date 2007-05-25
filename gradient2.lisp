46b08493ee97ee6e7502edb8d0d3da1d131dd3c4
ramarren <ramarren@16bf030e-5730-0410-874a-e6e7f71daa69>
2007-05-25 16:35:32 +0000

    Fix divide by zero bug and branch using closure
    git-svn-id: file:///home/SVNs/LispSvn/Gradient/trunk@80 16bf030e-5730-0410-874a-e6e7f71daa69

--- gradient2.lisp
+++ gradient2.lisp
@@ -11,23 +11,28 @@
 	(yd (- y2 y1)))
     (sqrt (+ (* xd xd) (* yd yd)))))
 
-(defun calc-col (d1 d2 c1 c2 dither)
-  (let ((d (+ d1 d2)))
-    (flet ((wav (a b)
-	     (/ (+ (* a d1)(* b d2)) d))
-	   (wav-dither (a b)
-	     (+ (/ (+ (* a d1)(* b d2)) d) (- (random dither) (/ dither 2)))))
-      (declare (inline wav wav-dither))
-      (mapcar #'(lambda (x) (max 0 (min x 255)))
-	      (mapcar #'round 
-		      (mapcar (if (zerop dither) #'wav #'wav-dither) c2 c1))))))
+(defun gen-calc-col (d c1 c2 dither)
+  (flet ((wav (a b d1 d2)
+	   (/ (+ (* a d1)(* b d2)) d))
+	 (wav-dither (a b d1 d2)
+	   (+ (/ (+ (* a d1)(* b d2)) d) (- (random dither) (/ dither 2)))))
+    (let ((wavf (if (zerop dither) #'wav #'wav-dither)))
+     #'(lambda (d1 d2)
+	 (declare (inline wav wav-dither))
+	 (if (> d1 d)
+	   c2
+	   (if (> d2 d)
+	     c1
+	     (mapcar #'(lambda (x y) (max 0 (min (round (funcall wavf x y d1 d2)) 255)))
+		     c2 c1)))))))
       
 
 (defun gradient-horiz (w h x1 x2 c1 c2 dither) 
   (declare (inline calc-col))
-  (let ((data (make-array (list w h))))
+  (let ((data (make-array (list w h)))
+	(calc-col (gen-calc-col (abs (- x2 x1)) c1 c2 0)))
     (dotimes (j h)
-      (let ((val (calc-col (abs (- j x1)) (abs (- j x2)) c1 c2 0)))
+      (let ((val (funcall calc-col (abs (- j x1)) (abs (- j x2)))))
 	(dotimes (i w)
 	  (setf (aref data i j) 
 		(mapcar #'(lambda (x) 
@@ -38,9 +43,10 @@
 
 (defun gradient-verti (w h y1 y2 c1 c2 dither)
   (declare (inline calc-col))
-  (let ((data (make-array (list w h))))
+  (let ((data (make-array (list w h)))
+	(calc-col (gen-calc-col (abs (- y2 y1)) c1 c2 0)))
     (dotimes (i w)
-      (let ((val (calc-col (abs (- i y1)) (abs (- i y2)) c1 c2 0)))
+      (let ((val (funcall calc-col (abs (- i y1)) (abs (- i y2)))))
 	(dotimes (j h)
 	  (setf (aref data i j) 
 		(mapcar #'(lambda (x) 
@@ -52,6 +58,7 @@
 (defun gradient-diag (w h x1 y1 x2 y2 col1 col2 dither)
   (declare (inline calc-col dist))
   (let ((data (make-array (list w h)))
+	(calc-col (gen-calc-col (dist x1 y1 x2 y2) col1 col2 dither))
 	(A (cond
 	     ((and (zerop x1)(zerop y1)) (/ y2 x2))
 	     ((and (zerop x2)(zerop y2)) (/ y1 x1))
@@ -72,10 +79,9 @@
 	(dotimes (i w)
 	  (dotimes (j h)
 	    (let ((V1 (+ (* (/ A B) i) j)))
-	      (setf (aref data i j) (calc-col 
+	      (setf (aref data i j) (funcall calc-col 
 				      (dist i j (* C1 (- V1 C2)) (- (* C3+ V1) C5))
-				      (dist i j (* C1 (- V1 C4)) (- (* C3+ V1) C6))
-				      col1 col2 dither)))))))
+				      (dist i j (* C1 (- V1 C4)) (- (* C3+ V1) C6)))))))))
     data))
 
 (defun gradient (w h x1 y1 x2 y2 c1 c2 &optional (dither 0)) 
